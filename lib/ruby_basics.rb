ffd1bf5e1506b54cbbd1fab960a27c0c03c40ad0
alanasim <alana.simoncelli@gmail.com>
2016-02-03 20:09:25 -0500

    My solutions

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,36 @@
 def division(num1, num2)
+  num1 / num2
  
 end
 
 def assign_variable(value)
+  name = value
   
 end
 
-def argue
+def argue(a)
+  a = "I'm right and you are wrong!"
   
 end
 
-def greeting
+def greeting(name,occupation)
+  puts ("Hello #{name}, I hear you are a #{occupation}")
 
 end
 
 def return_a_value
+  value = "Nice"
+  puts value
+  value
 
 end
 
 def last_evaluated_value
+  value = "expert" 
 
 end
 
-def pizza_party
+def pizza_party(topping="cheese")
+  topping
 
 end
